a0a7afdbaeaea21898ef0be8d4b832a9d810cc0c
Kashnikov-AV <egkashnikov@gmail.com>
2025-12-24 11:49:33 +0300

    update2

--- engine.py
+++ engine.py
@@ -4,34 +4,40 @@ from dataclasses import dataclass
 
 @dataclass
 class Initials:
-    # Define constants:
-    height = 80  # lattice dimensions
+    height = 80  # решетка
     width = 200
-    viscosity = 0.02  # fluid viscosity
+    viscosity = 0.02  # вязкость
     omega = 1 / (3 * viscosity + 0.5)  # "relaxation" parameter
-    u0 = 0.001  # initial and in-flow speed
+    u0 = 0.002  # initial and in-flow speed
     four9ths = 4.0 / 9.0  # abbreviations for lattice-Boltzmann weight factors
     one9th = 1.0 / 9.0
     one36th = 1.0 / 36.0
-    performanceData = False  # set to True if performance data is desired
 
 @dataclass
 class Data:
     # init data
-    initials = Initials()
+    ini = Initials()
     # Initialize all the arrays to steady rightward flow:
-    n0 = initials.four9ths * (np.ones((initials.height, initials.width)) - 1.5 * initials.u0 ** 2)  # particle densities along 9 directions
-    nN = initials.one9th * (np.ones((initials.height, initials.width)) - 1.5 * initials.u0 ** 2)
-    nS = initials.one9th * (np.ones((initials.height, initials.width)) - 1.5 * initials.u0 ** 2)
-    nE = initials.one9th * (np.ones((initials.height, initials.width)) + 3 * initials.u0 + 4.5 * initials.u0 ** 2 - 1.5 * initials.u0 ** 2)
-    nW = initials.one9th * (np.ones((initials.height, initials.width)) - 3 * initials.u0 + 4.5 * initials.u0 ** 2 - 1.5 * initials.u0 ** 2)
-    nNE = initials.one36th * (np.ones((initials.height, initials.width)) + 3 * initials.u0 + 4.5 * initials.u0 ** 2 - 1.5 * initials.u0 ** 2)
-    nSE = initials.one36th * (np.ones((initials.height, initials.width)) + 3 * initials.u0 + 4.5 * initials.u0 ** 2 - 1.5 * initials.u0 ** 2)
-    nNW = initials.one36th * (np.ones((initials.height, initials.width)) - 3 * initials.u0 + 4.5 * initials.u0 ** 2 - 1.5 * initials.u0 ** 2)
-    nSW = initials.one36th * (np.ones((initials.height, initials.width)) - 3 * initials.u0 + 4.5 * initials.u0 ** 2 - 1.5 * initials.u0 ** 2)
-    # Initialize barriers:
-    barrier = np.zeros((initials.width, initials.height), bool)  # True wherever there's a barrier
-    
+    n0 = ini.four9ths * (np.ones((ini.width, ini.height)) - 1.5 * ini.u0 ** 2)
+    nN = ini.one9th *   (np.ones((ini.width, ini.height)) - 1.5 * ini.u0 ** 2)
+    nS = ini.one9th *   (np.ones((ini.width, ini.height)) - 1.5 * ini.u0 ** 2)
+    nE = ini.one9th *   (np.ones((ini.width, ini.height)) + 3 * ini.u0 + 4.5 * ini.u0 ** 2 - 1.5 * ini.u0 ** 2)
+    nW = ini.one9th *   (np.ones((ini.width, ini.height)) - 3 * ini.u0 + 4.5 * ini.u0 ** 2 - 1.5 * ini.u0 ** 2)
+    nNE = ini.one36th * (np.ones((ini.width, ini.height)) + 3 * ini.u0 + 4.5 * ini.u0 ** 2 - 1.5 * ini.u0 ** 2)
+    nSE = ini.one36th * (np.ones((ini.width, ini.height)) + 3 * ini.u0 + 4.5 * ini.u0 ** 2 - 1.5 * ini.u0 ** 2)
+    nNW = ini.one36th * (np.ones((ini.width, ini.height)) - 3 * ini.u0 + 4.5 * ini.u0 ** 2 - 1.5 * ini.u0 ** 2)
+    nSW = ini.one36th * (np.ones((ini.width, ini.height)) - 3 * ini.u0 + 4.5 * ini.u0 ** 2 - 1.5 * ini.u0 ** 2)
+    # инициализация барьера
+    barrier = np.zeros((ini.width, ini.height), bool)  # True баррьер
+    barrierN  = np.zeros((ini.width, ini.height), bool)
+    barrierS  = np.zeros((ini.width, ini.height), bool)
+    barrierE  = np.zeros((ini.width, ini.height), bool)
+    barrierW  = np.zeros((ini.width, ini.height), bool)
+    barrierNE = np.zeros((ini.width, ini.height), bool)
+    barrierNW = np.zeros((ini.width, ini.height), bool)
+    barrierSE = np.zeros((ini.width, ini.height), bool)
+    barrierSW = np.zeros((ini.width, ini.height), bool)
+
     @property
     def rho(self):
         return self.n0 + self.nN + self.nS + self.nE + self.nW + self.nNE + self.nSE + self.nNW + self.nSW  # macroscopic density
@@ -45,111 +51,131 @@ class Data:
         return (self.nN + self.nNE + self.nNW - self.nS - self.nSE - self.nSW) / self.rho  # macroscopic y velocity
 
     def update_U0(self, data):
-        self.initials.U0 = data / 1000
+        self.ini.U0 = data / 1000
 
     def update_viscosity(self, data):
-        self.initials.viscosity = data / 1000
-    
-    def set_barrier(self):
+        self.ini.viscosity = data / 1000
+
+    def reset_barrier(self):
+        self.barrier = np.zeros((self.ini.width, self.ini.height), bool)
+
+    def other_barriers(self):
+        self.barrierN  = np.roll(self.barrier,  1, axis=1)  # север
+        self.barrierS  = np.roll(self.barrier, -1, axis=1)  # юг
+        self.barrierE  = np.roll(self.barrier,  1, axis=0)  # etc.
+        self.barrierW  = np.roll(self.barrier, -1, axis=0)
+        self.barrierNE = np.roll(self.barrierN, 1, axis=0)
+        self.barrierNW = np.roll(self.barrierN,-1, axis=0)
+        self.barrierSE = np.roll(self.barrierS, 1, axis=0)
+        self.barrierSW = np.roll(self.barrierS,-1, axis=0)
+
+    def set_simple_barrier(self):
+        ''' простой барьер '''
+        self.reset_barrier()
+        self.barrier[60, 29:49] = True
+        self.other_barriers()
+
+    def set_square_barrier(self):
+        ''' квадратный барьер '''
+        self.reset_barrier()
         self.barrier[40:61, 29] = True
         self.barrier[40:61, 49] = True
         self.barrier[40, 29:49] = True
         self.barrier[60, 29:49] = True
-        # self.barrier[int((self.initials.height / 2) - 8) : int((self.initials.height / 2) + 8, self.initials.height / 2)] = True  # simple linear barrier
-        self.barrierN  = np.roll(self.barrier,   1, axis=0)  # sites just north of barriers
-        self.barrierS  = np.roll(self.barrier,  -1, axis=0)  # sites just south of barriers
-        self.barrierE  = np.roll(self.barrier,   1, axis=1)  # etc.
-        self.barrierW  = np.roll(self.barrier,  -1, axis=1)
-        self.barrierNE = np.roll(self.barrierN,  1, axis=1)
-        self.barrierNW = np.roll(self.barrierN, -1, axis=1)
-        self.barrierSE = np.roll(self.barrierS,  1, axis=1)
-        self.barrierSW = np.roll(self.barrierS, -1, axis=1)
+        self.other_barriers()
+
+    def set_circle_barrier(self):
+        ''' цилиндрический барьер '''
+        self.reset_barrier()
+        x, y = np.ogrid[:self.ini.width, :self.ini.height]
+        dist_from_center = np.sqrt((x - 50)**2 + (y - 39)**2)
+        # Булева маска: True внутри круга
+        mask = (dist_from_center > 9) & (dist_from_center < 11)
+        self.barrier = mask
+        self.other_barriers()
         
     def draw_barrier(self):
-        bImageArray = np.zeros((self.initials.width, self.initials.height, 4), np.uint8)  # an RGBA image
+        bImageArray = np.zeros((self.ini.width, self.ini.height, 4), np.uint8)  # an RGBA image
         bImageArray[self.barrier] = [0, 0, 0, 255]
         bImageArray[~self.barrier] = [1, 1, 1, 0]
         barrierImage = pg.ImageItem(bImageArray)
         return barrierImage
     
-    # Move all particles by one step along their directions of motion (pbc):
+    # перемещение частиц вдоль их направления движения
     def stream(self):
-        self.nN  = np.roll(self.nN, 1, axis=0)  # axis 0 is north-south; + direction is north
-        self.nNE = np.roll(self.nNE, 1, axis=0)
-        self.nNW = np.roll(self.nNW, 1, axis=0)
-        self.nS  = np.roll(self.nS, -1, axis=0)
-        self.nSE = np.roll(self.nSE, -1, axis=0)
-        self.nSW = np.roll(self.nSW, -1, axis=0)
-        self.nE  = np.roll(self.nE, 1, axis=1)  # axis 1 is east-west; + direction is east
+        # Сохраняем СТАРЫЕ значения для bounce-back
+        nN_old = self.nN.copy()
+        nS_old = self.nS.copy()
+        nE_old = self.nE.copy()
+        nW_old = self.nW.copy()
+        nNE_old = self.nNE.copy()
+        nNW_old = self.nNW.copy()
+        nSE_old = self.nSE.copy()
+        nSW_old = self.nSW.copy()
+
+        # Распространение (правильно для вашей конвенции)
+        self.nN  = np.roll(self.nN,  1, axis=1)  # север: +Y
         self.nNE = np.roll(self.nNE, 1, axis=1)
-        self.nSE = np.roll(self.nSE, 1, axis=1)
-        self.nW  = np.roll(self.nW, -1, axis=1)
-        self.nNW = np.roll(self.nNW, -1, axis=1)
+        self.nNW = np.roll(self.nNW, 1, axis=1)
+
+        self.nS  = np.roll(self.nS,  -1, axis=1)  # юг: -Y
+        self.nSE = np.roll(self.nSE, -1, axis=1)
         self.nSW = np.roll(self.nSW, -1, axis=1)
-        # Use tricky boolean arrays to handle barrier collisions (bounce-back):
-        self.nN[self.barrierN.T] = self.nS[self.barrier.T]
-        self.nS[self.barrierS.T] = self.nN[self.barrier.T]
-        self.nE[self.barrierE.T] = self.nW[self.barrier.T]
-        self.nW[self.barrierW.T] = self.nE[self.barrier.T]
-        self.nNE[self.barrierNE.T] = self.nSW[self.barrier.T]
-        self.nNW[self.barrierNW.T] = self.nSE[self.barrier.T]
-        self.nSE[self.barrierSE.T] = self.nNW[self.barrier.T]
-        self.nSW[self.barrierSW.T] = self.nNE[self.barrier.T]
 
+        self.nE  = np.roll(self.nE,  1, axis=0)  # восток: +X
+        self.nNE = np.roll(self.nNE, 1, axis=0)
+        self.nSE = np.roll(self.nSE, 1, axis=0)
 
+        self.nW  = np.roll(self.nW,  -1, axis=0)  # запад: -X
+        self.nNW = np.roll(self.nNW, -1, axis=0)
+        self.nSW = np.roll(self.nSW, -1, axis=0)
 
+        # bounce-back (используем старые значения!)
+        self.nN[self.barrier] = nS_old[self.barrier]    # север ← юг
+        self.nS[self.barrier] = nN_old[self.barrier]    # юг ← север
+        self.nE[self.barrier] = nW_old[self.barrier]    # восток ← запад
+        self.nW[self.barrier] = nE_old[self.barrier]    # запад ← восток
+        self.nNE[self.barrier] = nSW_old[self.barrier]  # северо-восток ← юго-запад
+        self.nNW[self.barrier] = nSE_old[self.barrier]  # северо-запад ← юго-восток
+        self.nSE[self.barrier] = nNW_old[self.barrier]  # юго-восток ← северо-запад
+        self.nSW[self.barrier] = nNE_old[self.barrier]  # юго-запад ← северо-восток
 
-# def circle_mask(center_x, center_y, radius, array_shape):
-#     height, width = array_shape
-#     y, x = np.ogrid[:height, :width]
-#
-#     # Вычисляем расстояние до центра
-#     dist = np.sqrt((x - center_x)**2 + (y - center_y)**2)
-#
-#     # Окружность: точки на расстоянии ≈ radius
-#     # Используем толщину в 1 пиксель
-#     thickness = 1
-#     return (dist >= radius - thickness/2) & (dist <= radius + thickness/2)
-#
-# barrier = circle_mask(50, 50, 30, (height, width))
-
-    # Collide particles within each cell to redistribute velocities 
-    # (could be optimized a little more):
     def collide(self):
         ux2 = self.ux * self.ux  # pre-compute terms used repeatedly...
         uy2 = self.uy * self.uy
         u2 = ux2 + uy2
         omu215 = 1 - 1.5 * u2  # "one minus u2 times 1.5"
         uxuy = self.ux * self.uy
-        self.n0 = (1 - self.initials.omega) * self.n0 + self.initials.omega * self.initials.four9ths * self.rho * omu215
-        self.nN = (1 - self.initials.omega) * self.nN + self.initials.omega * self.initials.one9th * self.rho * (omu215 + 3 * self.uy + 4.5 * uy2)
-        self.nS = (1 - self.initials.omega) * self.nS + self.initials.omega * self.initials.one9th * self.rho * (omu215 - 3 * self.uy + 4.5 * uy2)
-        self.nE = (1 - self.initials.omega) * self.nE + self.initials.omega * self.initials.one9th * self.rho * (omu215 + 3 * self.ux + 4.5 * ux2)
-        self.nW = (1 - self.initials.omega) * self.nW + self.initials.omega * self.initials.one9th * self.rho * (omu215 - 3 * self.ux + 4.5 * ux2)
-        self.nNE = (1 - self.initials.omega) * self.nNE + self.initials.omega * self.initials.one36th * self.rho * (omu215 + 3 * (self.ux + self.uy) + 4.5 * (u2 + 2 * uxuy))
-        self.nNW = (1 - self.initials.omega) * self.nNW + self.initials.omega * self.initials.one36th * self.rho * (omu215 + 3 * (-self.ux + self.uy) + 4.5 * (u2 - 2 * uxuy))
-        self.nSE = (1 - self.initials.omega) * self.nSE + self.initials.omega * self.initials.one36th * self.rho * (omu215 + 3 * (self.ux - self.uy) + 4.5 * (u2 - 2 * uxuy))
-        self.nSW = (1 - self.initials.omega) * self.nSW + self.initials.omega * self.initials.one36th * self.rho * (omu215 + 3 * (-self.ux - self.uy) + 4.5 * (u2 + 2 * uxuy))
+        self.n0 = (1 - self.ini.omega) * self.n0 + self.ini.omega * self.ini.four9ths * self.rho * omu215
+        self.nN = (1 - self.ini.omega) * self.nN + self.ini.omega * self.ini.one9th * self.rho * (omu215 + 3 * self.uy + 4.5 * uy2)
+        self.nS = (1 - self.ini.omega) * self.nS + self.ini.omega * self.ini.one9th * self.rho * (omu215 - 3 * self.uy + 4.5 * uy2)
+        self.nE = (1 - self.ini.omega) * self.nE + self.ini.omega * self.ini.one9th * self.rho * (omu215 + 3 * self.ux + 4.5 * ux2)
+        self.nW = (1 - self.ini.omega) * self.nW + self.ini.omega * self.ini.one9th * self.rho * (omu215 - 3 * self.ux + 4.5 * ux2)
+        self.nNE= (1 - self.ini.omega) * self.nNE + self.ini.omega * self.ini.one36th * self.rho * (omu215 + 3 * (self.ux + self.uy) + 4.5 * (u2 + 2 * uxuy))
+        self.nNW = (1 - self.ini.omega) * self.nNW + self.ini.omega * self.ini.one36th * self.rho * (omu215 + 3 * (-self.ux + self.uy) + 4.5 * (u2 - 2 * uxuy))
+        self.nSE = (1 - self.ini.omega) * self.nSE + self.ini.omega * self.ini.one36th * self.rho * (omu215 + 3 * (self.ux - self.uy) + 4.5 * (u2 - 2 * uxuy))
+        self.nSW = (1 - self.ini.omega) * self.nSW + self.ini.omega * self.ini.one36th * self.rho * (omu215 + 3 * (-self.ux - self.uy) + 4.5 * (u2 + 2 * uxuy))
         # Force steady rightward flow at ends (no need to set 0, N, and S components):
-        self.nE[:, 0]  = self.initials.one9th * (1 + 3 *  self.initials.u0 + 4.5 * self.initials.u0 ** 2 - 1.5 * self.initials.u0 ** 2)
-        self.nW[:, 0]  = self.initials.one9th * (1 - 3 *  self.initials.u0 + 4.5 * self.initials.u0 ** 2 - 1.5 * self.initials.u0 ** 2)
-        self.nNE[:, 0] = self.initials.one36th * (1 + 3 * self.initials.u0 + 4.5 * self.initials.u0 ** 2 - 1.5 * self.initials.u0 ** 2)
-        self.nSE[:, 0] = self.initials.one36th * (1 + 3 * self.initials.u0 + 4.5 * self.initials.u0 ** 2 - 1.5 * self.initials.u0 ** 2)
-        self.nNW[:, 0] = self.initials.one36th * (1 - 3 * self.initials.u0 + 4.5 * self.initials.u0 ** 2 - 1.5 * self.initials.u0 ** 2)
-        self.nSW[:, 0] = self.initials.one36th * (1 - 3 * self.initials.u0 + 4.5 * self.initials.u0 ** 2 - 1.5 * self.initials.u0 ** 2)
+        self.nE[ 0, :] = self.ini.one9th  * (1 + 3 * self.ini.u0 + 4.5 * self.ini.u0 ** 2 - 1.5 * self.ini.u0 ** 2)
+        self.nW[ 0, :] = self.ini.one9th  * (1 - 3 * self.ini.u0 + 4.5 * self.ini.u0 ** 2 - 1.5 * self.ini.u0 ** 2)
+        self.nNE[0, :] = self.ini.one36th * (1 + 3 * self.ini.u0 + 4.5 * self.ini.u0 ** 2 - 1.5 * self.ini.u0 ** 2)
+        self.nSE[0, :] = self.ini.one36th * (1 + 3 * self.ini.u0 + 4.5 * self.ini.u0 ** 2 - 1.5 * self.ini.u0 ** 2)
+        self.nNW[0, :] = self.ini.one36th * (1 - 3 * self.ini.u0 + 4.5 * self.ini.u0 ** 2 - 1.5 * self.ini.u0 ** 2)
+        self.nSW[0, :] = self.ini.one36th * (1 - 3 * self.ini.u0 + 4.5 * self.ini.u0 ** 2 - 1.5 * self.ini.u0 ** 2)
+        # ----------------------------------------------------
+        self.nE[-1, :] = self.nE[-2, :]
+        self.nW[-1, :] = self.nW[-2, :]
+        self.nNE[-1, :] = self.nNE[-2, :]
+        self.nSE[-1, :] = self.nSE[-2, :]
+        self.nNW[-1, :] = self.nNW[-2, :]
+        self.nSW[-1, :] = self.nSW[-2, :]
+        # ------------------------------------------------------
 
     # Считаем вихри
     def curl(self):
-        c = np.roll(self.uy.T, -1, axis=1) - np.roll(self.uy.T, 1, axis=1) - np.roll(self.ux.T, -1, axis=0) + np.roll(self.ux.T, 1, axis=0)
-        return c
-#
-#
-# # Here comes the graphics and animation...
-# theFig = matplotlib.pyplot.figure(figsize=(10, 4))
-# # FIXME
-# fluidImage = matplotlib.pyplot.imshow(curl(ux, uy), origin='lower', norm=matplotlib.pyplot.Normalize(-.1, .1),
-#                                       cmap=matplotlib.pyplot.get_cmap(cmaps[8]), interpolation='none')
-# # See http://www.loria.fr/~rougier/teaching/matplotlib/#colormaps for other cmap options
-# bImageArray = np.zeros((height, width, 4), np.uint8)  # an RGBA image
-# bImageArray[barrier, 3] = 255  # set alpha=255 only at barrier sites
-# barrierImage = matplotlib.pyplot.imshow(bImageArray, origin='lower', interpolation='none')
+        dv_dx = np.roll(self.uy, -1, axis=0) - np.roll(self.uy, 1, axis=0)
+        # ∂u/∂y = производная ux по Y (axis=1)
+        du_dy = np.roll(self.ux, -1, axis=1) - np.roll(self.ux, 1, axis=1)
+        # Вихрь: ω = ∂v/∂x - ∂u/∂y
+        vorticity = dv_dx - du_dy
+        return vorticity
--- main.py
+++ main.py
@@ -9,8 +9,6 @@ import pyqtgraph as pg
 from utils import cmaps
 from engine import Data
 
-
-
 # по умолчанию фон графиков pyqtgraph чёрный, делаем его белым
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
@@ -25,43 +23,27 @@ class ExampleApp(widgets.QMainWindow, Design):
         
         # класс с данными и массивами
         self.data = Data()
-        
         # цвета кнопок
         self.bt_stop.setStyleSheet('background-color: rgb(255, 174, 174);')
         self.bt_stop.setEnabled(False)
         self.bt_start.setStyleSheet('background-color: rgb(191, 255, 190);')
         self.bt_reset.setStyleSheet('background-color: rgb(255, 255, 161);')
 
-        self.label_vel.setText(f'скорость потока = {self.data.initials.u0}')
-        self.label_vis.setText(f'вязкость = {self.data.initials.viscosity}')
+        self.label_vel.setText(f'скорость потока = {self.data.ini.u0}')
+        self.label_vis.setText(f'вязкость = {self.data.ini.viscosity}')
 
         # barrier type
-        self.select_prop.addItems(['Квадрат', 'Цилиндр', 'Профиль крыла']) # barrier type
+        self.select_prop.addItems(['без барьера', 'простой барьер', 'квадрат', 'цилиндр', 'профиль крыла']) # barrier type
         self.select_prop.currentIndexChanged.connect(self.set_barrier_type)
-        self.select_graph.addItems(['Плотность', 'x скорость', 'у скорость', 'скорость', 'завихрение']) # plot type
+        self.select_graph.addItems(['плотность', 'x скорость', 'у скорость', 'скорость', 'завихрение']) # plot type
         self.select_graph.currentIndexChanged.connect(self.set_plot_type)
         self.select_cmap.addItems(cmaps) # цветовые схемы
         self.select_cmap.currentIndexChanged.connect(self.set_cmap) # обработка выбора меню cmap
-        
-        # рисование градиента
-        gradient_array = np.linspace(0, 1, 200).reshape(-1, 1) # 1 строка, 200 столбцов
-        gradient_array = np.repeat(gradient_array, 80, axis=1)  # повторяем 80 строк
-        
-        self.data.set_barrier()
-        br = self.data.draw_barrier()
-        br.setCompositionMode(mode=QPainter.CompositionMode.CompositionMode_Multiply)
-        
-        self.img_item = pg.ImageItem(gradient_array)
-        self.img_item.setColorMap(pg.colormap.get('plasma', source='matplotlib'))
-        self.canvas.clear()
+
+        # self.data.set_simple_barrier()
+        self.img_item = pg.ImageItem(self.set_plot_type())
         self.canvas.addItem(self.img_item)
-        self.canvas.addItem(br)
-        # self.canvas.setCompositionMode(QPainter.CompositionMode.CompositionMode_Darken)
-        self.canvas.showGrid(x=False, y=False)
-        self.canvas.hideAxis('left')
-        self.canvas.hideAxis('bottom')
-        
-        
+
         # self.data.set_barriers()
         # self.canvas.addItem(self.data.draw_barriers())
         
@@ -70,15 +52,13 @@ class ExampleApp(widgets.QMainWindow, Design):
         self.timer.timeout.connect(self.step)
         self.timer.stop()
 
-
         self.bt_start.clicked.connect(self.set_start)
         self.bt_stop.clicked.connect(self.set_stop)
+        self.bt_reset.clicked.connect(self.set_reset)
         self.bt_step.clicked.connect(self.step)
         self.slider_velocity.valueChanged.connect(self.set_velocity)
         self.slider_viscosity.valueChanged.connect(self.set_viscosity)
-        
-        
-        
+
         # рисование стрелочки с силой 
         # drawForceArrow(barrierxSum/barrierCount, barrierySum/barrierCount, barrierFx, barrierFy);
 
@@ -99,41 +79,55 @@ class ExampleApp(widgets.QMainWindow, Design):
         self.img_item.setColorMap(pg.colormap.get(cmap, source='matplotlib'))
         
     def set_barrier_type(self):
-        barrier = self.select_prop.currentText()
-        if barrier == 'Квадрат':
+        barr = self.select_prop.currentText()
+        if barr == 'простой барьер':
+            self.data.set_simple_barrier()
+            self.draw_plot()
+        elif barr == 'квадрат':
+            self.data.set_square_barrier()
+            self.draw_plot()
+        elif barr == 'цилиндр':
+            self.data.set_circle_barrier()
+            self.draw_plot()
+        elif barr == 'профиль крыла':
             pass
-        elif barrier == 'Цилиндр':
-            pass
-        elif barrier == 'Профиль крыла':
-            pass
+        elif barr == 'без барьера':
+            self.data.reset_barrier()
+            self.draw_plot()
     
     def set_plot_type(self):
         plot = self.select_graph.currentText()
-        print(plot)
         if plot == 'Плотность':
-            return self.data.rho.T
+            return self.data.rho
         elif plot == 'x скорость':
-            return self.data.ux.T
+            return self.data.ux
         elif plot == 'у скорость':
-            return self.data.uy.T
+            return self.data.uy
         elif plot == 'скорость':
-            return self.data.ux.T + self.data.uy.T
+            return np.sqrt(self.data.ux**2 + self.data.uy**2)
         elif plot == 'завихрение':
             return self.data.curl()
-        
-        
-        # db_spin_velocity
+
+
+
     def step(self):
         # Эта функция вызывается АВТОМАТИЧЕСКИ каждые 50 мс
-        # print('Таймер сработал!')
-        self.data.stream();
-        self.data.collide();
-        graphic = self.set_plot_type()
-        self.img_item = pg.ImageItem(graphic)
-        self.canvas.clear()
-        self.canvas.addItem(self.img_item)
-        self.set_cmap()
-        # self.canvas.addItem(br)
+        print('Таймер сработал!')
+        self.data.collide()
+        self.data.stream()
+        self.draw_plot()
+
+        # Проверка средних скоростей
+        mean_ux = np.mean(self.data.ux)
+        mean_uy = np.mean(self.data.uy)
+        print(f"Шаг: средняя ux={mean_ux:.6f}, uy={mean_uy:.6f}")
+
+        # Проверка на левой и правой границах
+        left_flow = np.mean(self.data.nE[0, :] - self.data.nW[0, :])
+        right_flow = np.mean(self.data.nE[-1, :] - self.data.nW[-1, :])
+        print(f"  Поток слева: {left_flow:.6f}, справа: {right_flow:.6f}")
+
+        print(f"u0 = {self.data.ini.u0}")
 
     def set_start(self):
         self.bt_step.setEnabled(False)
@@ -145,7 +139,19 @@ class ExampleApp(widgets.QMainWindow, Design):
         self.timer.start(50)
         print('start')
         self.step()
-        
+
+    def set_reset(self):
+        self.data.n0  = self.data.ini.four9ths *(np.ones((self.data.ini.width, self.data.ini.height)) - 1.5 * self.data.ini.u0 ** 2)
+        self.data.nN  = self.data.ini.one9th *  (np.ones((self.data.ini.width, self.data.ini.height)) - 1.5 * self.data.ini.u0 ** 2)
+        self.data.nS  = self.data.ini.one9th *  (np.ones((self.data.ini.width, self.data.ini.height)) - 1.5 * self.data.ini.u0 ** 2)
+        self.data.nE  = self.data.ini.one9th *  (np.ones((self.data.ini.width, self.data.ini.height)) + 3  *  self.data.ini.u0 + 4.5 * self.data.ini.u0 ** 2 - 1.5 * self.data.ini.u0 ** 2)
+        self.data.nW  = self.data.ini.one9th *  (np.ones((self.data.ini.width, self.data.ini.height)) - 3  *  self.data.ini.u0 + 4.5 * self.data.ini.u0 ** 2 - 1.5 * self.data.ini.u0 ** 2)
+        self.data.nNE = self.data.ini.one36th * (np.ones((self.data.ini.width, self.data.ini.height)) + 3  *  self.data.ini.u0 + 4.5 * self.data.ini.u0 ** 2 - 1.5 * self.data.ini.u0 ** 2)
+        self.data.nSE = self.data.ini.one36th * (np.ones((self.data.ini.width, self.data.ini.height)) + 3  *  self.data.ini.u0 + 4.5 * self.data.ini.u0 ** 2 - 1.5 * self.data.ini.u0 ** 2)
+        self.data.nNW = self.data.ini.one36th * (np.ones((self.data.ini.width, self.data.ini.height)) - 3  *  self.data.ini.u0 + 4.5 * self.data.ini.u0 ** 2 - 1.5 * self.data.ini.u0 ** 2)
+        self.data.nSW = self.data.ini.one36th * (np.ones((self.data.ini.width, self.data.ini.height)) - 3  *  self.data.ini.u0 + 4.5 * self.data.ini.u0 ** 2 - 1.5 * self.data.ini.u0 ** 2)
+        self.draw_plot()
+
 
     def set_stop(self):
         self.bt_step.setEnabled(True)
@@ -154,11 +160,15 @@ class ExampleApp(widgets.QMainWindow, Design):
         self.bt_stop.setEnabled(False)
         self.timer.stop()
         print('stop')
-            
 
-
-
-
+    def draw_plot(self):
+        self.img_item = pg.ImageItem(self.set_plot_type())
+        self.canvas.clear()
+        self.canvas.addItem(self.img_item)
+        br = self.data.draw_barrier()
+        br.setCompositionMode(mode=QPainter.CompositionMode.CompositionMode_Multiply)
+        self.canvas.addItem(br)
+        self.set_cmap()
 
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
     app = widgets.QApplication(sys.argv)  # Новый экземпляр QApplication
